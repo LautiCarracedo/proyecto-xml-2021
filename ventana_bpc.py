40616b4a3c4cae4b6905699e4df89336bde9fdde
Lauti Carracedo <lauti.carracedo@hotmail.com>
2022-01-25 13:45:33 -0300

    agregando excepcion

--- ventana_bpc.py
+++ ventana_bpc.py
@@ -190,10 +190,9 @@ class VentanaBPC:
                 else:
                     messagebox.showerror(message="El codigo de barra 1 debe ser numerico, empezar con 04 y contener al menos 42 caracteres Revise que no tenga espacios al final ni un enter demas", title="Error")
 
-        except(UnboundLocalError):
-            messagebox.showerror(message="Debe ingresar en la lista de tipo pago al menos un tipo E(electronico) y un tipo P(presencial). De lo contrario si todos los pagos son de un solo tipo, debe elegir dicha opcion en formato correspondiente", title="Error")
-        #except(ValueError):
-        #    messagebox.showerror(message="El codigo de barra debe ser numerico y contener al menos 42 caracteres Revise que no tenga espacios al final", title="Error")
+        
+        except:
+            messagebox.showerror(message="Excepcion no controlada", title="Error")
 
     def ocultar_mostrar_codbarra_segun_formato(self, event):
         tipo_pago_selec = str(self.cbbox_opc.get())
