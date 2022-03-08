ed09eebdd267bf3d3efd940e096076f4890e7ac4
Lauti Carracedo <lauti.carracedo@hotmail.com>
2022-03-08 10:22:09 -0300

    tratando de refactorizar las formas de pago y sus acciones

---
+++ tipo_pago.py
@@ -0,0 +1,27 @@
+class FormaPago():
+    def tipo_pago_selec(self) -> str:
+        pass
+
+class PagoPresencial(FormaPago):
+    def __init__(self) -> str:
+        super().__init__()
+        self.forma_pago = "Pagos presenciales"
+    
+    def tipo_pago_selec(self):
+        return self.forma_pago
+
+class PagoElectronico(FormaPago):
+    def __init__(self) -> str:
+        super().__init__()
+        self.forma_pago = "Pagos electronicos"
+    
+    def tipo_pago_selec(self):
+        return self.forma_pago
+
+def tipo_pago_selec(forma_pago : FormaPago):
+    print(forma_pago.tipo_pago_selec())
+
+
+
+#valor = PagoPresencial()
+#tipo_pago_selec(valor)
